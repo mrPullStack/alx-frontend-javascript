d61b09240a5c7d0c53cba211f8d02c7772017525
sungu <cliffland.jasungu@gmail.com>
2025-07-20 14:14:18 +0300

    create functions specific to employees

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -46,3 +46,14 @@ function createEmployee (salary: number | string): Director | Teacher {
     }
     return new Director();
 }
+
+function isDirector(employee: Director | Teacher): employee is Director {
+    return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Director | Teacher): string {
+    if (isDirector(employee)){
+        return employee.workDirectorTasks();
+    }
+    return (employee as Teacher).workTeacherTasks();
+}
