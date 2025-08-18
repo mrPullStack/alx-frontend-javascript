ed8d992aa32631e0ea53110736d769e8aed0d648
SuNgu <cliffland.jasungu@gmail.com>
2025-08-18 10:37:13 +0300

    add required files

---
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -0,0 +1,9 @@
+// Define type RowID
+export type RowID = number;
+
+// Define interface RowElement
+export interface RowElement {
+    firstName: string;
+    lastName: string;
+    age?: number; // optional
+}
